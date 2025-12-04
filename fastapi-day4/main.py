178969702194c0604f29f37c3e1b483ffcdabe46
RishiAnand108 <risanand108@gmail.com>
2025-12-04 08:26:21 +0530

    day 4 learning

---
+++ fastapi-day4/main.py
@@ -0,0 +1,82 @@
+from fastapi import FastAPI,HTTPException
+from pydantic import BaseModel
+from typing import Optional,List
+
+app=FastAPI()
+
+
+# ---------------------------
+# MODEL
+# ---------------------------
+
+class Product(BaseModel):
+    name: str
+    price: float
+    in_stock: bool = True
+    description: Optional[str] = None
+
+#in-memory database
+
+product_db={ }    # key=product_id, value = Product
+
+
+# ---------------------------
+# CREATE (POST)
+# ---------------------------
+
+@app.post('/products')
+def create_product(product_id: int, product: Product):
+    if product_id in product_db:
+        raise HTTPException(status_code=400,detail='Product ID already exists')
+    
+    product_db[product_id] = product
+    return {'messeage':'Product created','id':product_id,'data':product}
+# ---------------------------
+# READ - GET ALL PRODUCTS
+# ---------------------------
+
+@app.get('/products')
+def get_all_products():
+    return {'count':len(product_db),'products':product_db}
+
+# ---------------------------
+# READ - GET SINGLE PRODUCT
+# ---------------------------
+
+@app.get('')
+def get_product(product_id: int):
+    if product_id not in product_db:
+        raise HTTPException(status_code=404, detail='Product not found')
+    
+    return product_db[product_id]
+
+# ---------------------------
+# UPDATE - ENTIRE PRODUCT (PUT)
+# ---------------------------
+
+@app.put('/products/{product_id}')
+def update_product(product_id:int, product: Product):
+    if product_id not in product_db:
+        raise HTTPException(status_code=404, detail='Product not found')
+    
+    product_db[product_id] = product
+    return {'message': "product updated", 'updated_data':product}
+
+
+# ---------------------------
+# PARTIAL UPDATE (PATCH)
+# ---------------------------
+
+class ProductUpdate(BaseModel):
+    name: Optional[str] = None
+    price: Optional[float] = None
+    in_stock: Optional[bool] = None
+    description: Optional[str] = None
+
+
+@app.patch('/products/{product_id}')
+def partial_update(product_id : int, product_data: ProductUpdate):
+    if product_id not in product_db:
+        raise HTTPException(status_code=404, detail='Product not found')
+
+    stored_product    
